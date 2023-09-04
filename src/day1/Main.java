0a232e3a13aac379db4409da23f734567967f1a0
Aurio Rajaa <riorajaa2018@gmail.com>
2023-09-04 13:40:10 +0700

    first commit

---
+++ src/day1/Main.java
@@ -0,0 +1,23 @@
+package day1;
+
+public class Main {
+    public static void main(String[] args) {
+        // Primitive Data Types
+        int age = 25; // Integer
+        char grade = 'A'; // Character
+        boolean isStudent = true; // Boolean
+        String name = "John"; // String
+
+        // Non-Primitive Data Type (String is a non-primitive)
+        String greeting = "Hello, ";
+        String message = greeting + name;
+
+        // Printing the values
+        System.out.println("Age: " + age);
+        System.out.println("Grade: " + grade);
+        System.out.println("Is a student: " + isStudent);
+        System.out.println("Name: " + name);
+        System.out.println("Greeting: " + greeting);
+        System.out.println("Message: " + message);
+    }
+}
